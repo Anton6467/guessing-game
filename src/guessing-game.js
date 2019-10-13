c121c6f1fbfe24e01513c6cf2d1eef4785d5687b
Anton <anton6467@gmail.com>
2019-10-13 12:34:21 +0400

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.maximum;
+        this.minimum;
+        this.middle;
+    }
 
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max;
     }
 
     guess() {
-
+        this.middle = Math.ceil ((this.minimum + this.maximum)/2);
+        return this.middle;
     }
 
     lower() {
 
-    }
+        if (this.maximum > this.middle) {
+            this.maximum = this.middle;
+        }
+}
 
     greater() {
-
+        if (this.minimum < this.middle) {
+            this.minimum = this.middle;
+        }
     }
 }
+
 
 module.exports = GuessingGame;
